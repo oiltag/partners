133e6afba89edca05f613a9d524890d9e8e5e91d
thiago2santos <thisanti@hotmail.com>
2024-05-11 11:20:16 -0300

    Partners project creation

---
+++ src/test/java/br/com/oiltag/partners/PartnersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oiltag.partners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
